51fb557644e48c00413af255a370bebadbfae0b9
浦田　幸嗣 <btz0727@me.com>
2017-09-06 16:16:57 +0900

    Add require method and change the update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,18 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-      user = User.find(params[:id])
-      user_id == current_user.id ? user.update(user_params) : redirect_to root_path
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      destroy_user_session_path
+    end
   end
 
     private
     def user_params
-      params.permit(:name, :email)
+      params.require(:user).permit(:name, :email)
     end
 end
