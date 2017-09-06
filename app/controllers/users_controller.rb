290b8fcce02fad4d3359992923314ed29d46ea32
浦田　幸嗣 <btz0727@me.com>
2017-09-06 17:53:19 +0900

    Add render :edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if current_user.update(user_params)
       redirect_to root_path
     else
-      edit_user_registration_path
+      render :edit
     end
   end
 
