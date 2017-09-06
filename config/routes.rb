a46c0e0568f627d918b6f1dd58f4000f74f8791f
浦田　幸嗣 <btz0727@me.com>
2017-09-06 16:20:02 +0900

    Add delete action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :delete]
 end
