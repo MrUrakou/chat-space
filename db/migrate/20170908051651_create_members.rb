36c320db4af78dad85209b29c470358bc8562dab
浦田　幸嗣 <btz0727@me.com>
2017-09-08 14:21:07 +0900

    Revert "Add user_id and group_id"
    This reverts commit db4e2b5905e5895a5a4ec04fdb3bdbe17482af31.

--- db/migrate/20170908051651_create_members.rb
+++
@@ -1,9 +0,0 @@
-class CreateMembers < ActiveRecord::Migration[5.1]
-  def change
-    create_table :members do |t|
-      t.integer :user_id, null: false, add_index: true
-      t.integer :group_id, null: false
-      t.timestamps
-    end
-  end
-end
