5301a53a36ce73312b6353df733fb366a6c71b70
浦田　幸嗣 <btz0727@me.com>
2017-09-08 14:32:47 +0900

    Add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :name, :email, presence: true, uniqueness: true
+
+  has_many :groups, through: :members
+  has_many :messages
 end
