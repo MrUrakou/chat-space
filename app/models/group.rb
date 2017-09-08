5e6b9241fb90e136c8ac97c6f1be36afd9443c39
浦田　幸嗣 <btz0727@me.com>
2017-09-08 19:07:43 +0900

    Add validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ApplicationRecord
   has_many :members
   has_many :users, through: :members
   accepts_nested_attributes_for :members, allow_destroy: true
+  validates :name, presence: true
 end
