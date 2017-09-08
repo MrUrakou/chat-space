ecfaabeb2215d46edbf8dcdda6a70d47d58460cb
浦田　幸嗣 <btz0727@me.com>
2017-09-08 19:07:20 +0900

    Add user_id and group_id

---
+++ db/migrate/20170908051651_create_members.rb
@@ -0,0 +1,9 @@
+class CreateMembers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :members do |t|
+      t.integer :user_id, null: false, add_index: true
+      t.integer :group_id, null: false
+      t.timestamps
+    end
+  end
+end
