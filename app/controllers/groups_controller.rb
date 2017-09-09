79486de8ccd043875cc8f9a88a0bc03bba3b11b4
浦田　幸嗣 <btz0727@me.com>
2017-09-09 17:55:08 +0900

    Add or create index, edit, update action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,30 @@
 class GroupsController < ApplicationController
 
+  def index
+    @groups = Group.all
+    @group = Group.find_by(params[:id])
+  end
+
   def new
     @group = Group.new
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    group = Group.find(params[:id])
+      group.update(group_params)
+  end
+
   def create
-    Group.create(name: group_params[:name])
-    redirect_to root_path
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
     private
