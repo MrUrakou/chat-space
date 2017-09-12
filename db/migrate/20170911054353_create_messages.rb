e0e2d7cbb7915417d276569078b1625949938761
浦田　幸嗣 <btz0727@me.com>
2017-09-12 16:59:29 +0900

    Edit message table

--- db/migrate/20170911054353_create_messages.rb
+++ db/migrate/20170911054353_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-      t.integer :user_id, null: false, foreign_key: true
       t.string :text
       t.string :image
-      t.integer :group_id, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
       t.timestamps
     end
   end
