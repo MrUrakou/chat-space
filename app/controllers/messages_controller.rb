2649a0d85ca8c5926d9b0f7e902558d00a421751
浦田　幸嗣 <btz0727@me.com>
2017-09-12 17:00:22 +0900

    Edit merge

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,28 +1,27 @@
 class MessagesController < ApplicationController
+
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @groupname = "#{@group.name}"
     @members = @group.users
-  end
-
-  def new
     @message = Message.new
   end
 
   def update
-    @group.update(group_params)
+    @message.update(message_params)
   end
 
   def create
+    @group = Group.find(params[:group_id])
     @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path
+      redirect_to group_messages_path
     end
   end
 
   private
   def message_params
-    params.require(:message).permit(:text, :image, :group_id)
+    params.require(:message).permit(:text, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
