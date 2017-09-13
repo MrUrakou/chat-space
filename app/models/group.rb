fd71dca436f5a764b1cb76cc9b9bc08c09dec02b
浦田　幸嗣 <btz0727@me.com>
2017-09-13 20:03:48 +0900

    Create message_display method

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,16 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :messages
   validates :name, presence: true
+
+  def message_display
+    if messages.present?
+      if messages.last.text.present?
+        messages.last.text
+      else
+        "画像が投稿されています。"
+      end
+    else
+      "メッセージはありません。"
+    end
+  end
 end
