508561ef4ef62f14143dfe1ef2e0b59b5ea71b99
浦田　幸嗣 <btz0727@me.com>
2017-09-15 17:25:58 +0900

    Add validates group_id, user_id

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
-  validates :text_or_image, presence: true
+  validates :text_or_image, :group_id, :user_id, presence: true
   private
   def text_or_image
     text.presence or image.presence
