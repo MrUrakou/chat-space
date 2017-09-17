cd0a72d1ec30f2c86c314535ad0165af840d76b3
浦田　幸嗣 <btz0727@me.com>
2017-09-17 18:09:30 +0900

    Add sequence

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,14 +3,11 @@ FactoryGirl.define do
   pass = "testtest"
 
   factory :user do
-    name                  "urata"
-    email                 "btz@gmail.com"
+    sequence(:name) {|n| "#{n}taro" }
+    sequence(:email) {|n| "example_#{n}@example.com" }
     password              pass
     password_confirmation pass
-
-    after(:create) do |user|
-      create(:group_user, user: user, group: create(:group))
-    end
   end
-
 end
+
+
