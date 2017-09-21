e49a66156ebcceebcf450fd8647428b887bbe1f4
浦田　幸嗣 <btz0727@me.com>
2017-09-21 16:23:32 +0900

    Edit image url

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,12 @@
 $(function(){
   function buildHTML(message){
 
+  if (message.image == null){
+    image_html = ``
+  } else {
+    image_html = `<div class = "message-bottom"><img src ="${message.image}" width="120" height="180"}</div>`
+  }
+
     var html = `<div class="message-top">
                   <div class="message-top__name">
                     ${message.name}
@@ -10,7 +16,7 @@ $(function(){
                   </div>
                   <div class="message-bottom">
                     ${message.text}
-                    ${message.image}
+                    ${image_html}
                   </div>
                 </div>`
     return html;
@@ -18,6 +24,7 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
+    console.log;
     var url = $(this).attr('action')
     $.ajax({
       url: url,
@@ -30,9 +37,10 @@ $(function(){
     .done(function(data){
       var html = buildHTML(data);
       $('.contents__messages').append(html)
+      $('.contents__form-image').val('')
       $('.contents__form-field').val('')
       $(".button").attr('disabled', false);
-      $('.contents__messages').animate({scrollTop:$('.contents__form').offset().top});
+      $('.contents__messages').animate({scrollTop: $('.contents__messages')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
       alert('error');
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
-Jbuilder.ignore_nil
 json.text  @message.text
-json.image  @message.image
+json.image  @message.image.url
 json.name   @message.user.name
 json.created_at @message.created_at.strftime("%Y/%m/%d/ %H:%M")
+
